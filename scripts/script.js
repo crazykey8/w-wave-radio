ecfc69409acae2f9dbd816822a27355286a1c06e
Sergei <korolnyy2@mail.ru>
2022-09-24 12:16:42 +0300

    javascript & guest add * fixed bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,72 @@
+
+document.querySelector('#burger').addEventListener('click', () => {
+  document.querySelector('.burger').classList.toggle('open');
+  document.querySelector('.header__top:before').classList.toggle('header__top-active')
+  document.querySelector('.header__top:after').classList.toggle('header__top-active')
+  document.querySelector('.top__nav').classList.toggle('header__top-active')
+});
+
+document.querySelector('.top__search').addEventListener('click', (e) => {
+  document.querySelector('.top__input').classList.toggle('top__form-visible');
+  e.preventDefault();
+});
+
+const btnPlayaudio = document.querySelectorAll('.btn__playaudio')
+for (let button of btnPlayaudio) {
+  button.addEventListener("click", e => {
+    let activeBtn = document.querySelector('button.pause')
+    if (activeBtn && activeBtn !== e.currentTarget) {
+      activeBtn.classList.remove('pause')
+    }
+    button.classList.toggle('pause')
+  })
+}
+
+const btnMore = document.querySelector('.podcast__btn');
+const podcastItems = document.querySelectorAll('.podcast__item');
+btnMore.addEventListener('click', () => {
+  podcastItems.forEach(el => {
+    el.classList.add('podcast__item-visible')
+  });
+  btnMore.closest('.podcast__next').classList.add('podcast__next-hidden');
+})
+
+const btnEvent = document.querySelectorAll('.podcast__event')
+for (let button of btnEvent) {
+  button.addEventListener("click", e => {
+    let activeBtnEvent = document.querySelector('button.pause')
+    if (activeBtnEvent && activeBtnEvent !== e.currentTarget) {
+      activeBtnEvent.classList.remove('pause')
+    }
+    button.classList.toggle('pause')
+  })
+}
+
 const element = document.querySelector('.broadcast__author-select');
 const choices = new Choices(element, {
   position: 'down',
   searchEnabled: false,
   shouldSort: false,
   itemSelectText: '',
+});
+
+$(".accordion-list").accordion({
+  heightStyle: "content",
+  collapsible: true,
+  icons: false,
+});
+
+document.querySelectorAll('.blogger__name').forEach(function (tabsBtn) {
+  tabsBtn.addEventListener('click', function (e) {
+    const path = e.currentTarget.dataset.path;
+
+    document.querySelectorAll('.blogger__name').forEach(function (btn) {
+      btn.classList.remove('blogger__name-active')
+    });
+    e.currentTarget.classList.add('blogger__name-active');
+    document.querySelectorAll('.tab__item').forEach(function (tabsBtn) {
+      tabsBtn.classList.remove('tab__item-active')
+    });
+    document.querySelector(`[data-target="${path}"]`).classList.add('tab__item-active');
+  });
 });
